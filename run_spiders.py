85c9dff34088699e4c7a77038317a3a2203d8f36
Eray Alp <erayalp.e@gmail.com>
2024-05-13 23:24:23 +0300

    add run script

---
+++ run_spiders.py
@@ -0,0 +1,12 @@
+import os
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir("market_scraper")
+os.makedirs("data", exist_ok=True)
+os.chdir("data")
+
+spiders = ["migros", "carrefour", "mopas", "sokmarket", "marketpaketi"]
+
+for spider in spiders:
+    os.system(f"scrapy crawl {spider}")
