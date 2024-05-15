cfc085445014c1b180ad17c0193f8c262438ad7e
Eray Alp <erayalp.e@gmail.com>
2024-05-15 12:56:23 +0300

    add download delay

--- market_scraper/spiders/carrefour.py
+++ market_scraper/spiders/carrefour.py
@@ -21,7 +21,8 @@ class CarrefourSpider(scrapy.Spider):
                     "export_empty_fields": True,
                 }
             }
-        }
+        },
+        "DOWNLOAD_DELAY": 1
     }
     user_agent_list = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 "
